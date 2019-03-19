0f3e1d05dcb9a10dc4f12e9f36c0d1009d95f260
shuoGG1239 <328893769@qq.com>
2019-03-19 20:54:02 +0800

    修复无法上传中文文件的问题

--- util/sm_util.py
+++ util/sm_util.py
@@ -1,22 +1,24 @@
-import requests
 import json
 import os
+import urllib
+
+import requests
 
 
 def upload_img(image_path):
     files_map = [('smfile', image_path)]
+    file_name = os.path.split(image_path)[1]
     params = {
         'ssl': False,
         'format': 'png'
     }
-    multi_files = list(map(lambda x: (x[0], (os.path.split(x[1])[1], open(x[1], 'rb'))), files_map))
+    multi_files = list(map(lambda x: (x[0], (urllib.parse.quote(os.path.split(x[1])[1]), open(x[1], 'rb'))), files_map))
     resp = requests.post("https://sm.ms/api/upload", data=params, files=multi_files)
     print(resp.text)
     result = json.loads(resp.text)
     if result['code'] == 'success':
         view_url = result['data']['url']
         del_url = result['data']['delete']
-        file_name = result['data']['filename']
         resp_dict = {'url': view_url, 'delete': del_url, 'filename': file_name, 'success': True}
         print(view_url)
         print(del_url)
